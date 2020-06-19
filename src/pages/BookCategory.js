a161729ce6d36ee9122f1db1610e59bf855c2fbf
richard <richard.turra@sensedata.com.br>
2020-06-19 15:17:42 -0300

    category view

--- src/pages/BookCategory.js
+++ src/pages/BookCategory.js
@@ -1,7 +1,25 @@
 import React from "react";
+import ShelfContainer from "../components/ShelfContainer";
+import BookItem from "../components/BookItem";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 
 export default function BookCategory() {
   const { category } = useParams();
-  return <div>Category {category}</div>;
+  const books = useSelector(state =>
+    state.books.filter(book => book.category === category)
+  );
+  return (
+    <ShelfContainer>
+      {books.length > 0 ? (
+        <div>
+          {books.map(book => {
+            return <BookItem key={book.id} book={book} />;
+          })}
+        </div>
+      ) : (
+        <div>no books in the category {category}</div>
+      )}
+    </ShelfContainer>
+  );
 }

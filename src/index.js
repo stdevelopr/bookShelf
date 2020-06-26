3e98d7057c114918f5c31447faf2bc7cbe7a640c
richard <richard.turra@sensedata.com.br>
2020-06-25 21:02:20 -0300

    favicon

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
+import Favicon from "react-favicon";
+import favicon from "./favicon.png";
 
 const store = configureStore();
 ReactDOM.render(
   <Provider store={store}>
+    <Favicon url={favicon} />
     <Router>
       <App />
     </Router>
